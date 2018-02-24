254213f7cf3cf3aca29e842837d31b5e19922bd5
OATOMO <443437061@qq.com>
2018-02-24 22:30:52 +0800

    add cali option

--- peace_love_and_linux/calibration/operationcalibration.cpp
+++ peace_love_and_linux/calibration/operationcalibration.cpp
@@ -7,8 +7,20 @@ operationCalibration::operationCalibration(std::vector<cv::Mat>  m_saveImageAll,
 	ui(new Ui::operationCalibration)
 {
 	ui->setupUi(this);
+	initOptionUi();
+}
+
+void operationCalibration::initOptionUi(){
 	m_cabOption.w = ui->wSpinBox->value();
 	m_cabOption.h = ui->hSpinBox->value();
+	m_cabOption.squareSize = ui->squareSizeSpinBox->value();
+	m_cabOption.aspectRatio = ui->aspectRatioSpinBox->value();
+	on_patternComboBox_activated(ui->patternComboBox->currentText());
+	flags = 0;
+
+	//ui
+//	ui->aspectRatioSpinBox->setDisabled(true);
+	on_aspectRatioCheckBox_stateChanged(ui->aspectRatioCheckBox->isChecked());
 }
 
 operationCalibration::~operationCalibration()
@@ -26,6 +38,50 @@ void operationCalibration::on_hSpinBox_valueChanged(int arg1)
 	m_cabOption.h = arg1;
 }
 
+void operationCalibration::on_squareSizeSpinBox_valueChanged(double arg1)
+{
+	m_cabOption.squareSize = arg1;
+	qDebug() << m_cabOption.squareSize;
+}
+
+void operationCalibration::on_aspectRatioSpinBox_valueChanged(double arg1)
+{
+	m_cabOption.aspectRatio = arg1;
+	qDebug() << m_cabOption.aspectRatio;
+}
+
+void operationCalibration::on_patternComboBox_activated(const QString &arg1)
+{
+	QMap<QString,int> m_map;
+	m_map.insert("CHESSBOARD",1);
+	m_map.insert("CIRCLES_GRID",2);
+	m_map.insert("ASYMMETRIC_CIRCLES_GRID",3);
+
+	switch((int)m_map.value(arg1)){
+		case 1:
+			m_cabOption.pattern = CHESSBOARD;
+		break;
+		case 2:
+			m_cabOption.pattern = CIRCLES_GRID;
+		break;
+		case 3:
+			m_cabOption.pattern = ASYMMETRIC_CIRCLES_GRID;
+		break;
+	}
+	qDebug() << m_cabOption.pattern;
+}
+
+void operationCalibration::on_aspectRatioCheckBox_stateChanged(int arg1)
+{
+	if(arg1){
+		ui->aspectRatioSpinBox->setEnabled(true);
+	}
+	else{
+		ui->aspectRatioSpinBox->setDisabled(true);
+	}
+}
+
+
 void operationCalibration::on_caliPushButton_clicked()
 {
 	for(int i = 0;i < m_saveImageAll.size();i++){
@@ -35,12 +91,97 @@ void operationCalibration::on_caliPushButton_clicked()
 
 		cv::findChessboardCorners(proImage,cv::Size(m_cabOption.w,m_cabOption.h),tmpPoint,
 								  cv::CALIB_CB_ADAPTIVE_THRESH | cv::CALIB_CB_FAST_CHECK | cv::CALIB_CB_NORMALIZE_IMAGE);
-		qDebug("Pix (%f,%f)",tmpPoint[0].x,tmpPoint[0].y);
+		qDebug("%d: Pix (%f,%f)",i,tmpPoint[0].x,tmpPoint[0].y);
 //		cv::drawChessboardCorners(proImage,cv::Size(m_cabOption.w,m_cabOption.h),tmpPoint,true);
 //		cv::imshow(QString::number(i,10).toStdString(),proImage);
-		cv::TermCriteria termCriteria = cv::TermCriteria(cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS, 30, 0.01);
-		cv::cornerSubPix(proImage, tmpPoint, cvSize(5,5), cvSize(-1,-1), termCriteria);
-		qDebug("SubPix (%f,%f)",tmpPoint[0].x,tmpPoint[0].y);
-		qDebug() << tmpPoint.size();
+//		cv::TermCriteria termCriteria = cv::TermCriteria(cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS, 30, 0.1);
+		cv::cornerSubPix(proImage, tmpPoint, cvSize(11,11), cvSize(-1,-1), cv::TermCriteria(cv::TermCriteria::EPS+cv::TermCriteria::COUNT, 30, 0.1 ));
+		imagePoints.push_back(tmpPoint);
+		qDebug("%d: SubPix (%f,%f)",i,tmpPoint[0].x,tmpPoint[0].y);
+//		qDebug() << tmpPoint.size();
+
+
+		cv::drawChessboardCorners(proImage,cv::Size(m_cabOption.w,m_cabOption.h),tmpPoint,true);
+		cv::resize(proImage,proImage,cv::Size(320,240),0,0,cv::INTER_LINEAR);
+		QImage viewTemp(proImage.data,proImage.cols,proImage.rows,QImage::Format_Indexed8);
+		this->ui->imageLabel->setPixmap(QPixmap::fromImage(viewTemp));
+
 	}
+
+	if(ui->aspectRatioCheckBox->isChecked()){
+		flags |= cv::CALIB_FIX_ASPECT_RATIO;
+	}
+	if(ui->zeroTangentCheckBox){
+		flags |= cv::CALIB_ZERO_TANGENT_DIST;
+	}
+	if(ui->principalPointCheckBox){
+		flags |= cv::CALIB_FIX_PRINCIPAL_POINT;
+	}
+
+
+	cameraMatrix = cv::Mat::eye(3,3,CV_64F);
+	if(flags & cv::CALIB_FIX_ASPECT_RATIO){
+		cameraMatrix.at<double>(0,0) = m_cabOption.aspectRatio;
+	}
+
+	distCoeffs = cv::Mat::zeros(8,1,CV_64F);
+
+	objectPoints.resize(1);
+	calcChessboardCorners(cv::Size(m_cabOption.w,m_cabOption.h),
+						  m_cabOption.squareSize,
+						  objectPoints[0],
+						  m_cabOption.pattern);
+	objectPoints.resize(imagePoints.size(),objectPoints[0]);
+
+//	qDebug() << "objectPoints.size()" << objectPoints.size();
+
+	/*
+		@objectPoints:	世界坐标系中的点,由calcChessboardCorners求出	应该输入vector< vector< Point3f > >
+		@imagePoings:	其对应的图像中的点,							应该输入vector< vector< Point3f > >
+		@imageSize:		图像的大小，在计算相机的内参数和畸变矩阵需要用到
+		@cameraMatrix:	内参数矩阵。输入一个Mat cameraMatrix即可
+		@distCoeffs:	畸变矩阵。输入一个Mat distCoeffs即可
+		rvecs:			旋转向量,应该输入一个Mat的vector,即vector<Mat> rvecs因为每个vector<Point3f>会得到一个rvecs
+		tvecs:			位移向量,和rvecs一样，也应该为vector tvecs
+	*/
+	double rms = calibrateCamera(objectPoints, imagePoints, m_saveImageAll[0].size(),
+			cameraMatrix,distCoeffs, rvecs, tvecs, flags|cv::CALIB_FIX_K4|cv::CALIB_FIX_K5);
+						///*|CALIB_FIX_K3*/|CALIB_FIX_K4|CALIB_FIX_K5);
+	qDebug("RMS error reported by calibrateCamera: %g\n", rms);
 }
+
+
+void operationCalibration::calcChessboardCorners(cv::Size boardSize, float squareSize,
+				std::vector<cv::Point3f>& corners, Pattern patternType)
+{
+	corners.resize(0);
+
+	switch (patternType) {
+		case CHESSBOARD:
+		case CIRCLES_GRID:
+			for(int i = 0;i < boardSize.height;i++){
+				for(int j = 0;j < boardSize.width;j++){
+					corners.push_back(cv::Point3f(float(j*squareSize),
+												  float(i*squareSize),0));
+				}
+			}
+			break;
+
+		case ASYMMETRIC_CIRCLES_GRID:
+			for( int i = 0; i < boardSize.height; i++ ){
+				for( int j = 0; j < boardSize.width; j++ ){
+					corners.push_back(cv::Point3f(float((2*j + i % 2)*squareSize),
+												  float(i*squareSize), 0));
+				}
+			}
+			break;
+
+		default:
+			CV_Error(cv::Error::StsBadArg,"Unkown pattern type\n");
+			break;
+		}
+
+}
+
+
+
--- peace_love_and_linux/calibration/operationcalibration.h
+++ peace_love_and_linux/calibration/operationcalibration.h
@@ -5,6 +5,8 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <QDebug>
+#include <QMap>
+#include <unistd.h>
 
 namespace Ui {
 	class operationCalibration;
@@ -16,12 +18,12 @@ struct CabOption{
 	uint w;					//the number of inner corners per one of board dimension
 	uint h;					//the number of inner corners per another board dimension
 	Pattern pattern; 		//the type of pattern: chessboard or circles' grid
-	int squareSize;     	//square size in some user-defined units (1 by default)
+	float squareSize;     	//square size in some user-defined units (1 by default)
 	cv::Mat outCameraParams;//the output filename for intrinsic [and extrinsic] parameters
 	//						//write detected feature points
 	//						//write extrinsic parameters
 	//						//assume zero tangential distortion
-	//						//fix aspect ratio (fx/fy)
+	float aspectRatio;		//fix aspect ratio (fx/fy)
 	//						//fix the principal point at the center
 	//						//flip the captured images around the horizontal axis
 };
@@ -34,17 +36,36 @@ public:
 	explicit operationCalibration(std::vector<cv::Mat>  m_saveImageAll,QWidget *parent = 0);
 	~operationCalibration();
 //fun
-
+	void initOptionUi();
+	void calcChessboardCorners(cv::Size boardSize, float squareSize, std::vector<cv::Point3f>& corners, Pattern patternType = CHESSBOARD);
 //data
-	std::vector<cv::Mat>  m_saveImageAll;
-	CabOption m_cabOption;
+	std::vector<cv::Mat>  m_saveImageAll;				//需要标定的原始图片
+	CabOption m_cabOption;								//标定参数集
+	std::vector<std::vector<cv::Point3f> > objectPoints;	//世界坐标系中的点,由calcChessboardCorners求出
+	std::vector<std::vector<cv::Point2f> > imagePoints;	//其对应的图像中的点
+	cv::Mat cameraMatrix;								//内参矩阵
+	cv::Mat distCoeffs;								//畸变矩阵
+	std::vector<cv::Mat> rvecs;						//旋转向量
+	std::vector<cv::Mat> tvecs;						//位移向量
+	int flags;
 
 private slots:
 	void on_wSpinBox_valueChanged(int arg1);
 
 	void on_hSpinBox_valueChanged(int arg1);
 
+	void on_squareSizeSpinBox_valueChanged(double arg1);
+
+	void on_aspectRatioSpinBox_valueChanged(double arg1);
+
+	void on_patternComboBox_activated(const QString &arg1);
+
 	void on_caliPushButton_clicked();
+
+
+
+
+	void on_aspectRatioCheckBox_stateChanged(int arg1);
 
 private:
 	Ui::operationCalibration *ui;
